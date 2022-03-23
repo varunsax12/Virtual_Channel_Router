951465d3aa4701b45274509cf8688c21be7a3ee4
sandilyabvh <balemarthysandilya@gmail.com>
2022-03-23 02:13:43 -0400

    Allocator top

---
+++ rtl/libs/allocator_top.sv
@@ -0,0 +1,40 @@
+// Description: Top module to select allocator type
+
+`include "VR_define.vh"
+
+module allocator_top #(
+  parameter NUM_REQS = 4,
+  parameter NUM_RESS = 4
+) (
+    // Standard
+    input wire                 clk,
+    input wire                 reset,
+    // 'NUM_RESS' requests for each requestor (out of NUM_REQS)
+    input wire  [NUM_RESS-1:0] requests [NUM_REQS-1:0],
+    // 'NUM_REQS' grants for each resource (out of NUM_RESS)
+    output wire [NUM_RESS-1:0] grants [NUM_REQS-1:0]
+);
+    if (`ALLOCATOR_TYPE == `WAVEFRONT_ALLOCATOR) begin
+        allocator_wavefront #(
+            .NUM_REQS(NUM_REQS)
+            .NUM_RESS(NUM_RESS)
+        ) allocator (
+            .clk(clk),
+            .reset(reset),
+            .requests(requests),
+            .grants(grants)
+        );
+    end
+    else begin
+        allocator_separable #(
+            .NUM_REQS(NUM_REQS)
+            .NUM_RESS(NUM_RESS)
+        ) allocator (
+            .clk(clk),
+            .reset(reset),
+            .requests(requests),
+            .grants(grants)
+        );
+    end
+
+endmodule
