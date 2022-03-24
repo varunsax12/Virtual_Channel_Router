183a5ad61d1438f5b048dfbcdb91521447b781ce
Saxena <vsaxena36@ece-linlabsrv01.ece.gatech.edu>
2022-03-24 02:32:19 -0400

    Updated router top and tb

--- rtl/router_top.sv
+++ rtl/router_top.sv
@@ -6,15 +6,56 @@
 module router_top #(
     parameter NUM_PORTS = 5,
     parameter NUM_VC    = 4,
+    parameter NUM_ROUTERS = 16,
+    parameter ROUTER_PER_ROW = 4,
+    parameter ROUTER_ID = 0,
+    parameter ROUTER_ID_BITS = $clog2(NUM_ROUTERS),
     parameter PORT_BITS = $clog2(NUM_PORTS),
     parameter VC_BITS = $clog2(NUM_VC)
 ) (
     input  logic   clk, reset,
-    input  logic   [NUM_PORTS-1:0]        dst_port [NUM_VC*NUM_PORTS-1:0],
     input  logic   [NUM_VC*NUM_PORTS-1:0] vc_availability,
     output logic   [VC_BITS-1:0]          vc_index [NUM_PORTS-1:0],
     output logic   [NUM_PORTS-1:0]        vc_read_valid
 );
+
+    /************************************
+    *          VC                       *
+    ************************************/
+    reg [`FLIT_DATA_WIDTH-1:0]      vc_buffer [NUM_PORTS-1:0][NUM_VC-1:0];
+
+
+    /************************************
+    *          Route compute            *
+    ************************************/
+
+    logic   [NUM_PORTS-1:0]        dst_port [NUM_VC*NUM_PORTS-1:0];
+
+    localparam DIR_BITS = 3; // N, S, W, E, Eject
+    localparam DIR_ONE_HOT = 2**DIR_BITS;
+    for (genvar i = 0; i < NUM_PORTS; ++i) begin
+        for (genvar j = 0; j < NUM_VC; ++j) begin
+            logic [DIR_BITS-1:0] vc_direction;
+            route_compute # (
+                .NUM_ROUTERS(NUM_ROUTERS),
+                .ROUTER_PER_ROW(ROUTER_PER_ROW)
+            ) rc (
+                .current_router(ROUTER_ID_BITS'(ROUTER_ID)),
+                .dest_router(vc_buffer[i][j][`FLIT_DATA_WIDTH-1-:ROUTER_ID_BITS]),
+                .direction(vc_direction)
+            );
+
+            // One hot encode the direction
+            logic [DIR_ONE_HOT-1:0]  one_hot_direction;
+            index_2_one_hot #(
+                .NUM_BITS(DIR_BITS)
+            ) dir2port (
+                .index(vc_direction),
+                .out_one_hot(one_hot_direction)
+            );
+            assign dst_port[i*NUM_VC+j] = one_hot_direction[NUM_PORTS-1:0];
+        end
+    end
 
     /************************************
     *       VC Allocation              *
--- testbench/tb_router_top.sv
+++ testbench/tb_router_top.sv
@@ -5,13 +5,14 @@
 
 `timescale 10ns/1ns
 
+`include "VR_define.vh"
+
 module tb_vc_allocator;
     parameter NUM_PORTS = 5;
     parameter NUM_VCS = 4;
     parameter PORT_BITS = $clog2(NUM_PORTS);
     parameter VC_BITS = $clog2(NUM_VCS);
     reg clk, rstn;
-    reg [NUM_PORTS-1:0] dst_port [NUM_VCS*NUM_PORTS-1:0];
     reg [NUM_VCS*NUM_PORTS-1:0] vc_availability;
     wire [NUM_VCS*NUM_PORTS-1:0] allocated_ip_vcs [NUM_VCS*NUM_PORTS-1:0];
     logic   [VC_BITS-1:0]          vc_index [NUM_PORTS-1:0];
@@ -27,22 +28,33 @@ module tb_vc_allocator;
     ) rt (
         .clk(clk),
         .reset(rstn),
-        .dst_port(dst_port),
         .vc_availability(vc_availability),
         .vc_index(vc_index),
         .vc_read_valid(vc_read_valid)
     );
 
-    for(genvar i=0; i<NUM_PORTS; i++) begin
-        for(genvar j=0; j<NUM_VCS; j++) begin
-            assign wv_allocated_ip_vcs[i*NUM_VCS+j] = allocated_ip_vcs[i*NUM_VCS+j];
-            assign wv_dst_ports[i*NUM_VCS+j] = dst_port[i*NUM_VCS+j];
-        end
-    end
+    // for(genvar i=0; i<NUM_PORTS; i++) begin
+    //     for(genvar j=0; j<NUM_VCS; j++) begin
+    //         assign wv_allocated_ip_vcs[i*NUM_VCS+j] = allocated_ip_vcs[i*NUM_VCS+j];
+    //         assign wv_dst_ports[i*NUM_VCS+j] = dst_port[i*NUM_VCS+j];
+    //     end
+    // end
 
-    initial begin
-        #5000 $finish;
-    end
+    // initial begin
+    //     //$monitor("requests[0]:%b dable[0]:%b, weight_mat[1][1]:%b",mta.requests[0], mta.dable[0],mta.weight_mat[1][1]);
+    //     $monitor("\n\ntime:%0t,dst_port[0]:%b,dst_port[1]:%b,vc_availability=%b", $time,rt.dst_port[0],rt.dst_port[1],vc_availability,
+    //     "\nrt.vca.available_op_vcs[0] for ipvc0:%b,rt.vca.available_op_vcs[1] for ipvc1:%b", rt.vca.available_op_vcs[0], rt.vca.available_op_vcs[1],
+    //     "\nrt.vca.allocated_ip_vcs[0] for opvc0:%b,rt.vca.allocated_ip_vcs[1] for opvc1:%b", rt.vca.allocated_ip_vcs[0], rt.vca.allocated_ip_vcs[1],
+    //     "\nrt.vca.allocated_ip_vcs[2] for opvc2:%b,rt.vca.allocated_ip_vcs[3] for opvc3:%b", rt.vca.allocated_ip_vcs[2], rt.vca.allocated_ip_vcs[3],
+    //     "\nrt.vca.allocated_ip_vcs[4] for opvc4:%b,rt.vca.allocated_ip_vcs[5] for opvc5:%b", rt.vca.allocated_ip_vcs[4], rt.vca.allocated_ip_vcs[5],
+    //     "\nrt.vca.allocated_ip_vcs[6] for opvc6:%b,rt.vca.allocated_ip_vcs[7] for opvc7:%b", rt.vca.allocated_ip_vcs[6], rt.vca.allocated_ip_vcs[7],
+    //     "\nrt.vca.allocated_ip_vcs[8] for opvc8:%b,rt.vca.allocated_ip_vcs[9] for opvc9:%b", rt.vca.allocated_ip_vcs[8], rt.vca.allocated_ip_vcs[9],
+    //     "\nrt.vca.allocated_ip_vcs[10] for opvc10:%b,rt.vca.allocated_ip_vcs[11] for opvc11:%b", rt.vca.allocated_ip_vcs[10], rt.vca.allocated_ip_vcs[11],
+    //     "\nrt.vca.allocated_ip_vcs[12] for opvc12:%b,rt.vca.allocated_ip_vcs[13] for opvc13:%b", rt.vca.allocated_ip_vcs[12], rt.vca.allocated_ip_vcs[13],
+    //     "\nrt.vca.allocated_ip_vcs[14] for opvc14:%b,rt.vca.allocated_ip_vcs[15] for opvc15:%b", rt.vca.allocated_ip_vcs[14], rt.vca.allocated_ip_vcs[15],
+    //     "\nrt.vca.allocated_ip_vcs[16] for opvc16:%b,rt.vca.allocated_ip_vcs[17] for opvc17:%b", rt.vca.allocated_ip_vcs[16], rt.vca.allocated_ip_vcs[17],
+    //     "\nrt.vca.allocated_ip_vcs[18] for opvc18:%b,rt.vca.allocated_ip_vcs[19] for opvc19:%b", rt.vca.allocated_ip_vcs[18], rt.vca.allocated_ip_vcs[19]);
+    // end
 
     initial begin
         $dumpfile("test.vcd");
@@ -50,42 +62,54 @@ module tb_vc_allocator;
         clk = 0;
         @(negedge clk) rstn = 0;
         @(negedge clk) rstn = 1;
-        foreach(dst_port[i]) begin
-            if(i==0 || i==1)
-                if(i==0)
-                    dst_port[i] = 1; //00001
-                else
-                    dst_port[i] = 8; //00010
-            else
-                dst_port[i] = 0;
+        @(negedge clk) rstn = 0;
+        // foreach(dst_port[i]) begin
+        //     if(i==0 || i==1)
+        //         if(i==0)
+        //             dst_port[i] = 1; //00001
+        //         else
+        //             dst_port[i] = 8; //00010
+        //     else
+        //         dst_port[i] = 0;
+        // end
+        for (int i = 0; i < NUM_PORTS; ++i) begin
+            for (int j = 0; j < NUM_VCS; ++j) begin
+                rt.vc_buffer[i][j][`FLIT_DATA_WIDTH-1-:4] = $urandom%16;
+                $display("Port = %d, VC = %d, dest = %d", i, j, rt.vc_buffer[i][j][`FLIT_DATA_WIDTH-1-:4]);
+            end
         end
         foreach(vc_availability[i]) begin
             vc_availability[i] = 1;
         end
-        #20
-        @(negedge clk) begin
-            vc_availability[0] = 0;
-        end
-        for (int i = 0; i < NUM_PORTS; ++i) begin
-            for (int j = 0; j < NUM_VCS; ++j) begin
-                $display("Port req, Port = %d, VC = %d, %b", i, j, dst_port[i*NUM_VCS+j]);
+        for (int i = 0; i < 10; ++i) begin
+            @(negedge clk);
+            // @(negedge clk) begin
+            //     vc_availability[0] = 0;
+            // end
+            for (int i = 0; i < NUM_PORTS; ++i) begin
+                for (int j = 0; j < NUM_VCS; ++j) begin
+                    $display("Port req, Port = %d, VC = %d, %b", i, j, rt.dst_port[i*NUM_VCS+j]);
+                end
             end
-        end
-        for (int i = 0; i < NUM_PORTS; ++i) begin
-            for (int j = 0; j < NUM_VCS; ++j) begin
-                $display("VC alloc, %d, %b", i, rt.allocated_ip_vcs[i*NUM_VCS+j]);
+            for (int i = 0; i < NUM_PORTS; ++i) begin
+                for (int j = 0; j < NUM_VCS; ++j) begin
+                    $display("VC alloc, %d, %b, %b", i, rt.vca.available_op_vcs[i*NUM_VCS+j], rt.allocated_ip_vcs[i*NUM_VCS+j]);
+                end
             end
-        end
-        for (int i = 0; i < NUM_PORTS; ++i) begin
-            for (int j = 0; j < NUM_VCS; ++j) begin
-                $display("VC alloc..2, %d, %b", i, rt.vc_grants[i*NUM_VCS+j]);
+            for (int i = 0; i < NUM_PORTS; ++i) begin
+                for (int j = 0; j < NUM_VCS; ++j) begin
+                    $display("VC alloc..2, %d, %b", i, rt.vc_grants[i*NUM_VCS+j]);
+                end
             end
-        end
-        for (int i = 0; i < NUM_PORTS; ++i) begin
-            $display("SAlloc, %d, %b", i, rt.allocated_ports[i]);
-        end
-        for (int i = 0; i < NUM_PORTS; ++i) begin
-            $display("vc index = %d, %b, %b", i, vc_index[i], vc_read_valid[i]);
+            for (int i = 0; i < NUM_PORTS; ++i) begin
+                $display("req2port , %d, %b", i, rt.port_req[i]);
+            end
+            for (int i = 0; i < NUM_PORTS; ++i) begin
+                $display("SAlloc, %d, %b", i, rt.allocated_ports[i]);
+            end
+            for (int i = 0; i < NUM_PORTS; ++i) begin
+                $display("vc index = %d, %b, %b", i, vc_index[i], vc_read_valid[i]);
+            end
         end
         $finish;
         //#20
