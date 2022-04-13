5d55e076446619f24a60266350ae3b4a7cfe7e77
sandilyabvh <balemarthysandilya@gmail.com>
2022-04-13 18:11:51 -0400

    gui launch

--- synthesis/rtl.tcl
+++ synthesis/rtl.tcl
@@ -1,10 +1,7 @@
 #**************************************************/
-#* Script for Cadence RTL Compiler synthesis      */
-#*                                                */
-#* To run: rc < rc.tcl                            */
-#*                                                */
-#* Author: Ivan Castellanos, OSU                  */
-#* ivan.castellanos@okstate.edu                   */
+#* Script for Cadence Genus Synthesis             */
+#* Author: Sandilya Balemarthy                    */
+#* Referenced from: Ivan Castellanos, OSU         */
 #**************************************************/
 
 #Setting osu pdk dir
@@ -61,6 +58,7 @@ report timing > timing.rep
 report gates  > cell.rep
 report power  > power.rep
 
+# Equivalent files being dumped
 write_hdl -mapped >  ${DESIGN}.vh
 write_sdc >  ${DESIGN}.sdc
 
@@ -70,5 +68,5 @@ puts \n
 puts "Check timing.rep, area.rep, gate.rep and power.rep for synthesis results"
 puts \n
  
-# quit
+# Launch GUI
 gui_show
