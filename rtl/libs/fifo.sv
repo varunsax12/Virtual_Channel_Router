b58a6f1672b2e9c7b716b0adc8c00619f6383266
Saxena <vsaxena36@ece-linlabsrv01.ece.gatech.edu>
2022-04-24 00:23:14 -0400

    Added FIFO module

---
+++ rtl/libs/fifo.sv
@@ -0,0 +1,58 @@
+// Description: Module for fifo
+// File Details
+//    Author: Varun Saxena
+//    GT id: 903562211
+
+module fifo #(
+    FIFO_DEPTH = 8,
+    DATA_WIDTH = `FLIT_DATA_WIDTH
+) (
+    input logic clk, reset, push, pop,
+    input logic [DATA_WIDTH-1:0]    indata,
+    output logic empty, full,
+    output logic [DATA_WIDTH-1:0]   outdata
+);
+
+    localparam POINTER_WIDTH = $clog2(FIFO_DEPTH) + 1;
+
+    // Registers for the fifo
+    reg [DATA_WIDTH-1:0]    buffer [FIFO_DEPTH-1:0];
+    // Pointers
+    logic [POINTER_WIDTH-1:0] wr_ptr, rd_ptr;
+    // Tracker for last action
+    logic last_write;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            wr_ptr     <= 0;
+            rd_ptr     <= 0;
+            last_write <= 0;
+        end
+        else if (push && !full) begin
+            buffer[wr_ptr] <= indata;
+            last_write     <= 1;
+            if (wr_ptr == (FIFO_DEPTH-1)) begin
+                wr_ptr     <= 0;
+            end
+            else begin
+                wr_ptr     <= wr_ptr + 1;
+            end
+        end
+        else if (pop && !empty) begin
+            // The FIFO has an active out to save cycles
+            //outdata    <= buffer[rd_ptr];
+            last_write <= 0;
+            if (rd_ptr == (FIFO_DEPTH-1)) begin
+                rd_ptr <= 0;
+            end
+            else begin
+                rd_ptr <= rd_ptr + 1;
+            end
+        end
+    end
+
+    assign outdata = buffer[rd_ptr];
+    assign empty = (wr_ptr == rd_ptr) && !last_write;
+    assign full  = (wr_ptr == rd_ptr) && last_write;
+
+endmodule
---
+++ testbench/libs/tb_fifo.sv
@@ -0,0 +1,47 @@
+// Description: Module for tb fifo
+// File Details
+//    Author: Varun Saxena
+//    GT id: 903562211
+
+`timescale 10ns/1ps
+
+`include "VR_define.vh"
+
+module tb_fifo();
+
+    logic clk, reset, push, pop, empty, full;
+    logic [`FLIT_DATA_WIDTH-1:0]    indata, outdata;
+
+    fifo uut(
+        .clk(clk),
+        .reset(reset),
+        .push(push),
+        .pop(pop),
+        .empty(empty),
+        .full(full),
+        .indata(indata),
+        .outdata(outdata)
+    );
+
+    always begin
+        clk = 1; #10;
+        clk = 0; #10;
+    end
+
+    initial begin
+        $dumpfile("test.vcd");
+        $dumpvars;
+        @(negedge clk) reset = 1;
+        @(negedge clk) reset = 0;
+        for (int i = 0; i < 8; ++i) begin
+            @(negedge clk) push = 1; indata = i;
+        end
+        @(negedge clk) push = 0;
+        for (int i = 0; i < 8; ++i) begin
+            @(negedge clk) pop = 1;
+            @(posedge clk) $display("FIFO out data = %d\n", outdata);
+        end
+        $finish;
+    end
+
+endmodule
